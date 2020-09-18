1778852a4128f08ded93370b810115129328d149
Dennis Hedegaard <dennis@dhedegaard.dk>
2020-09-18 20:06:58 +0200

    Update hyper.js

--- hyper.js
+++ hyper.js
@@ -149,7 +149,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: ["hyper-pane", "hyper-material-theme"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
